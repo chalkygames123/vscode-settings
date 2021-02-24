beb6b4f383858ce081dcdc372b8a0e1ea6bcff1f
Takuya Fukuju <chalkygames123@gmail.com>
2021-02-24 11:06:54 +0900

    Sort ln commands in install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")" || return
 
 VSCODE_DIR=~/Library/Application\ Support/Code/User
 
-ln -fs "$PWD/settings.json" "$VSCODE_DIR"
 ln -fs "$PWD/keybindings.json" "$VSCODE_DIR"
+ln -fs "$PWD/settings.json" "$VSCODE_DIR"
 ln -fs "$PWD/snippets/global.code-snippets" "$VSCODE_DIR/snippets"
