592c758b96f13d9f9ab95f5fcf76bddd4859bd18
Takuya Fukuju <chalkygames123@gmail.com>
2021-02-25 23:03:40 +0900

    Move the settings into a directory

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ cd "$(dirname "$0")" || return
 
 < extensions xargs -I {} code --install-extension {}
 
+SETTINGS_DIR=settings
 VSCODE_DIR=~/Library/Application\ Support/Code/User
 
-ln -fs "$PWD/keybindings.json" "$VSCODE_DIR"
-ln -fs "$PWD/settings.json" "$VSCODE_DIR"
-ln -fs "$PWD/snippets" "$VSCODE_DIR"
+ln -fs "$PWD/$SETTINGS_DIR/keybindings.json" "$VSCODE_DIR"
+ln -fs "$PWD/$SETTINGS_DIR/settings.json" "$VSCODE_DIR"
+ln -fs "$PWD/$SETTINGS_DIR/snippets" "$VSCODE_DIR"
